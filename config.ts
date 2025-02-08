ec900fe53a4eb0440023146e4b8d4d92c8a6a977
Samuel Huang <hi@sghuang.com>
2025-02-08 14:58:59 -0500

    Improve config.ts for Continue

--- config.ts
+++ config.ts
@@ -1,41 +1,70 @@
 /** Continue config */
+
 // @ts-ignore - can't find type definitions here
 import { execSync } from "child_process";
 
-function runShellCommand(command: string): string {
+const runShellCommand = (command: string): string => {
   try {
     return execSync(command, { encoding: "utf8" }).trim();
   } catch (error) {
-    console.error(`Error: ${(error as Error).message}`);
+    console.error("Error:", error);
     return "";
   }
-}
+};
 
 const siliconFlowKey = runShellCommand(
   "op read op://Personal/SiliconFlow/credential"
 );
+const voyageKey = runShellCommand(
+  "op read 'op://Personal/Voyage AI/credential'"
+);
 
-const DSR1 = {
-  title: "DeepSeek R1",
-  provider: "siliconflow",
-  model: "deepseek-ai/DeepSeek-R1",
-  apiKey: siliconFlowKey,
-  systemMessage:
-    "You are an expert software developer. You give helpful and concise responses.",
-};
-
-const DSV3 = {
-  title: "DeepSeek V3",
+const DS: Partial<ModelDescription> & { provider: string } = {
   provider: "siliconflow",
   model: "deepseek-ai/DeepSeek-V3",
   apiKey: siliconFlowKey,
+};
+
+const DSV3: ModelDescription = {
+  ...DS,
+  title: "DeepSeek V3",
+  model: "deepseek-ai/DeepSeek-V3",
   systemMessage:
     "You are an expert software developer. You give helpful and concise responses.",
 };
 
-// @ts-ignore
+const DSR1: ModelDescription = {
+  ...DS,
+  title: "DeepSeek R1",
+  model: "deepseek-ai/DeepSeek-R1",
+  systemMessage:
+    "You are an expert software developer. You give helpful and concise responses.",
+};
+
+const DSV25: ModelDescription = {
+  title: "DeepSeek V2.5",
+  provider: "siliconflow",
+  model: "deepseek-ai/DeepSeek-V2.5",
+  apiKey: siliconFlowKey,
+};
+
+const VoyageCode3: EmbeddingsProviderDescription = {
+  provider: "voyage",
+  model: "voyage-code-3",
+  apiKey: voyageKey,
+};
+
 export function modifyConfig(config: Config): Config {
   config.models.push(DSR1);
   config.models.push(DSV3);
+  config.tabAutocompleteModel = DSV25;
+  config.embeddingsProvider = VoyageCode3;
+  config.reranker = {
+    name: "voyage",
+    params: {
+      model: "rerank-2",
+      apiKey: voyageKey,
+    },
+  } as RerankerDescription;
   return config;
 }
