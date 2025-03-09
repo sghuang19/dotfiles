8c844580a98f957a800eea73224cc9721460375d
Samuel Huang <hi@sghuang.com>
2025-03-08 20:13:29 -0500

    fish: create fish configs

---
+++ fish/config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting ""
+fish_vi_key_bindings
+
+# PATH
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.bun/bin
+fish_add_path /opt/homebrew/bin
+
+starship init fish | source
+
+# ENV
+set -x EDITOR nvim
+set -x DOTS $HOME/dev/dotfiles
+set -x UV_PYTHON 3.13
+
+# ABBR
+abbr -a vi nvim
+abbr -a less "less --mouse"
+abbr -a dots "cd $DOTS"
+abbr -a t "tmux attach -t base || tmux new -s base"
