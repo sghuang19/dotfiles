66cc90174df22e7075786d0d75e8a32b1f8a4b35
Samuel Huang <hi@sghuang.com>
2025-03-09 16:34:22 -0400

    nvim: finalize image.nvim config

--- nvim/lua/plugins/image.lua
+++ nvim/lua/plugins/image.lua
@@ -1,13 +1,8 @@
--- requires imagemagick, ueberzugpp and config, and tmux config
+-- requires imagemagick and tmux config
 return {
-	{
-		"3rd/image.nvim",
-		build = false,
-		cond= false, -- FIXME: this plugin isn't working yet
-		opts = {
-			backend = "ueberzug",
-			processor = "magick_cli",
-			rocks = { hererocks = true },
-		},
-	},
+	"3rd/image.nvim",
+	build = "brew install imagemagick",
+	cond = not vim.g.neovide, -- not supported in neovide yet
+	lazy = true, -- delay until needed
+	opts = { processor = "magick_cli" },
 }
