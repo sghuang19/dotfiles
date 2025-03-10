c2fcaf989690e8549fad1677a0332cda73570472
Samuel Huang <hi@sghuang.com>
2025-03-10 00:25:53 -0400

    nvim: finalize neo-tree config
    * add window-picker
    
    * add source-selector
    
    * fix a bug in cmd
    
    * improve loading timing

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,49 +1,62 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- FIXME: this plugin isn't working yet
-	},
-	-- load lazily if no directory is opened
-	lazy = vim.fn.argc() == 0 or vim.fn.filereadable(vim.fn.argv(0)) == 1,
-	cmd = "Neotree",
-	keys = {
-		-- Neotree recommended key mappings
-		{
-			"\\", -- Neotree recommended / by default, which is used for searching
-			"<cmd>Neotree toggle current reveal_force_cwd<cr>",
-			desc = "Toggle Neotree (current dir)",
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim",  -- delay until needed
+			-- "s1n7ax/nvim-window-picker", -- delay until needed
 		},
-		{
-			"|",
-			"<cmd>Neotree reveal left<cr>",
-			desc = "Reveal file in Neotree",
-		},
-		{
-			"gd",
-			":Neotree float reveal_file=<cfile> reveal_force_cwd<cr>",
-			desc = "Show file in floating Neotree",
-		},
-		{
-			"<leader>b",
-			"<cmd>Neotree toggle show buffers right<cr>",
-			desc = "Toggle buffer explorer",
-		},
-		{
-			"<leader>s",
-			"<cmd>Neotree toggle float git_status<cr>",
-			desc = "Show Git status in Neotree",
-		},
-	},
-	opts = {
-		filesystem = { hijack_netrw_behavior = "open_current" },
-		window = {
-			width = 25,
-			mappings = {
-				["P"] = { "toggle_preview", config = { use_image_nvim = true } },
+		-- load eagerly if directory is opened
+		lazy = vim.fn.argc() == 0 or vim.fn.filereadable(vim.fn.argv(0)) == 1,
+		-- we want :e to always work, which can't be specified with cmd
+		event = "VeryLazy",
+		keys = {
+			-- Neotree recommended key mappings
+			{
+				"\\", -- Neotree recommends / by default, which is used for searching
+				"<Cmd>Neotree toggle current reveal_force_cwd<CR>",
+				desc = "Toggle Neotree (current dir)",
 			},
+			{
+				"|",
+				"<Cmd>Neotree reveal left<CR>",
+				desc = "Reveal file in Neotree",
+			},
+			{
+				"gd",
+				"<Cmd>Neotree float reveal reveal_force_cwd<CR>",
+				desc = "Show file in floating Neotree",
+			},
+			{
+				"<Leader>b",
+				"<Cmd>Neotree toggle show buffers right<CR>",
+				desc = "Toggle buffer explorer",
+			},
+			{
+				"<Leader>s",
+				"<Cmd>Neotree toggle float git_status<CR>",
+				desc = "Show Git status in Neotree",
+			},
+		},
+		opts = {
+			filesystem = { hijack_netrw_behavior = "open_current" },
+			window = {
+				width = 25,
+				mappings = {
+					["P"] = { "toggle_preview", config = { use_image_nvim = true } },
+				},
+			},
+			source_selector = { winbar = true }, -- statusline is taken
+		},
+	},
+	{
+		"s1n7ax/nvim-window-picker",
+		lazy = true, -- delay until needed
+		opts = {
+			hint = "floating-big-letter",
+			picker_config = { handle_mouse_click = true },
 		},
 	},
 }
